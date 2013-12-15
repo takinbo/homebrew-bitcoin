08bd111eda68c372d6a3f9a4c1e00f29870cc8a2
Bryan Stitt <bryan@stitthappens.com>
2013-12-15 12:32:47 -0800

    make ocl optional for vanitgen

--- vanitygen.rb
+++ vanitygen.rb
@@ -4,14 +4,24 @@ class Vanitygen < Formula
   homepage 'http://github.com/WyseNynja/vanitygen'
   head 'https://github.com/WyseNynja/vanitygen.git'
   url 'https://github.com/WyseNynja/vanitygen.git', :tag => '0.23-red'
+  version '0.23-red'
 
+  option 'without-ocl', "Don't build oclvanitygen or oclvanityminer"
+
+  depends_on 'curl' if build.with? 'ocl'  # todo: require with-ssl on curl?
   depends_on 'openssl'
   depends_on 'pcre'
 
   def install
-    system "make", "all"
-    %w[keyconv oclvanitygen oclvanityminer vanitygen].each do |binary|
-      system "strip", "#{binary}"
+    binaries = [
+      'keyconv',
+      'vanitygen',
+    ]
+    binaries << 'oclvanitygen' if build.with? 'ocl'
+    binaries << 'oclvanityminer' if build.with? 'ocl'
+
+    binaries.each do |binary|
+      system "make", binary
       bin.install binary
     end
   end
