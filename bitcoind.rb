868ac3a39a109c19aec011260719571b0694100a
Bryan Stitt <bryan@stitthappens.com>
2014-06-15 10:21:53 -0700

    bitcoind 0.9.2

--- bitcoind.rb
+++ bitcoind.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Bitcoind < Formula
   homepage 'http://bitcoin.org/'
-  url 'https://github.com/bitcoin/bitcoin.git', :tag => 'v0.9.1'
-  version '0.9.1'
+  url 'https://github.com/bitcoin/bitcoin.git', :tag => 'v0.9.2'
+  version '0.9.2'
   head 'https://github.com/bitcoin/bitcoin.git', :branch => 'master'
 
   depends_on 'automake'
